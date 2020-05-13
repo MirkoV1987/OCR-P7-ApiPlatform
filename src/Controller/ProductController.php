d85855a5e5c9f06f8ce8ceff4de719b816af96b0
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-05-13 22:58:06 +0200

    Update ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,30 +3,54 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+// use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+// use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class ProductController extends Controller
+
+class ProductController extends AbstractController
 {
-    /**
-     * @Route("/products/{id}", name="app_product_show")
-     */
-    public function showAction()
+    public function create(Request $request, EntityManagerInterface $em)
     {
         $product = new Product();
-        $product
-            ->setName('Samsung Galaxy')
-            ->setBrand('Samsung')
-            ->setDesciption('Ceci est le nouveau modèle de Samsung Galaxy')
-            ->setPrice(450)
+        $product->setName('Samsung Galaxy')
+                ->setBrand('Samsung')
+                ->setDescription('Ceci est le nouveau modèle de Samsung Galaxy')
+                ->setPrice(450)
+                ->setDateAdd(new \Datetime)
+                ->setProperties('RAM 250Mo', 'Lithium Battery');
 
-        ;
-        $data = $this->get('jms_serializer')->serialize($product, 'json');
+        $em->persist($product);
+        $em->flush();
 
-        $response = new Response($data);
-        $response->headers->set('Content-Type', 'application/json');
+        return new Response(sprintf('Product %s successfully created', $product->getName()));
+    }
 
-        return $response;
+    public function list(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $roles = $request->request->get('roles');
+
+        if (!$roles) {
+            $roles = json_encode([]);
+        }
+
+        $user = new User($username);
+        $user->setUsername('Mirko Venturi');  
+        $user->setEmail('mirkoventuri@gmail.com');
+        $user->setDateAdd(new \Datetime);  
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setRoles(($roles));
+        $em->persist($user);
+        $em->flush();
+
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
     }
 }
