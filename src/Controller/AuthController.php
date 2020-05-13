3dde1b601f586a4d620ab464d1f25bcb54638c26
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-05-13 22:57:38 +0200

    Upload AuthController

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+
+class AuthController extends AbstractController
+{
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $username = $request->request->get('username');
+        $password = $request->request->get('password');
+        $roles = $request->request->get('roles');
+
+        if (!$roles) {
+            $roles = json_encode([]);
+        }
+
+        $user = new User($username);
+        $user->setUsername('Mirko Venturi');  
+        $user->setEmail('mirkoventuri@gmail.com');
+        $user->setDateAdd(new \Datetime);  
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $user->setRoles(($roles));
+        $em->persist($user);
+        $em->flush();
+
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+}
