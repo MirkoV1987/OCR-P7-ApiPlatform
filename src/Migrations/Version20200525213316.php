3b3863498cbb70578f0af70bfb5aae98e3b42fa4
Mirko Venturi <mirkoventuri.web@gmail.com>
2020-05-26 21:31:38 +0200

    Update Migrations

--- src/Migrations/Version20200525213316.php
+++ src/Migrations/Version20200525213316.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200513155921 extends AbstractMigration
+final class Version20200525213316 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,7 @@ final class Version20200513155921 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE clients CHANGE username name VARCHAR(80) NOT NULL');
+        $this->addSql('ALTER TABLE users CHANGE client_id client_id INT NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20200513155921 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE clients CHANGE name username VARCHAR(80) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE users CHANGE client_id client_id INT DEFAULT NULL');
     }
 }
